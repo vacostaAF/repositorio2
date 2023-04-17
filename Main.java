51ec82706e4dd3941a5b408c914a860a91579278
vacostaAF <130365623+vacostaAF@users.noreply.github.com>
2023-04-17 13:49:36 +0200

    Update and rename main.java to Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-	  System.out.println("Computando");
+	//Nueva versión!!!!  
+	System.out.println("Computando");
         Salida.escribir();
         System.out.println("Fin de programa");
     }
