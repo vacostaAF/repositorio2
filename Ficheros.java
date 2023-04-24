1d9f4c6f8eeacbcc842eb5a67f3d94f8d87b2719
ismaelerpistolas <130365680+ismaelerpistolas@users.noreply.github.com>
2023-04-24 13:01:59 +0200

    Add files via upload

---
+++ Ficheros.java
@@ -0,0 +1,22 @@
+public class Salida {
+    static void escribir() {
+        byte CR = 13;
+        byte LF = 10;
+
+        try {
+            FileOutputStream fos = new FileOutputStream("fich1.bin");
+            fos.write((byte)'a');
+            fos.write((byte)'e');
+            fos.write((byte)'i');
+            fos.write(CR);
+            fos.write(LF);
+            fos.close();
+        }
+        catch (IOException e)
+        {
+            System.out.println(e);
+        }
+    }
+
+
+}
