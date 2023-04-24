0a5adee5a953adc39575ef63835bac7e19cecf63
Señor Atlantida Cidep <130365680+srAtlantida@users.noreply.github.com>
2023-04-24 14:04:31 +0200

    Update Mainficheros.java

--- Mainficheros.java
+++ Mainficheros.java
@@ -3,6 +3,6 @@ public class Main {
 	System.out.println("Computando");
         Salida.escribir();
         System.out.println("Computando");
-        //cambio
+        //cambio @fabiocidep
     }
 }
